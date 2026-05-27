d58edef28f7a62c3cc4adc4dcaebabd001273438
Weixuan-Ma-TP <282408000+Weixuan-Ma-TP@users.noreply.github.com>
2026-05-27 14:07:52 +0900

    追加機能を実装しました

--- members/Weixuan-Ma-TP/src/RemoteLedBuzzerAlarm.ino
+++ members/Weixuan-Ma-TP/src/RemoteLedBuzzerAlarm.ino
@@ -6,16 +6,18 @@
 #define BUZZER_PIN  8
 
 /* ===== リモコンコード ===== */
-/* 1ボタン = LED、2ボタン = ブザー、3ボタン = 停止 */
-#define CODE_LED     0xFF30CF
-#define CODE_BUZZER  0xFF18E7
-#define CODE_STOP    0xFF7A85
+/* 1ボタン = LED、2ボタン = ブザー、3ボタン = 停止、警告ボタン = 警告モード */
+#define CODE_LED      0xFF30CF
+#define CODE_BUZZER   0xFF18E7
+#define CODE_STOP     0xFF7A85
+#define CODE_WARNING  0xFFA25D
 
 /* ===== 状態定義 ===== */
-#define STATE_WAIT    0
-#define STATE_LED     1
-#define STATE_BUZZER  2
-#define STATE_STOP    3
+#define STATE_WAIT     0
+#define STATE_LED      1
+#define STATE_BUZZER   2
+#define STATE_STOP     3
+#define STATE_WARNING  4
 
 /* ===== 赤外線受信 ===== */
 IRrecv irrecv(IR_PIN);
@@ -27,13 +29,15 @@ int currentState = STATE_WAIT;
 unsigned long receivedCode = 0;
 unsigned long lastReceiveMillis = 0;
 unsigned long lastBlinkMillis = 0;
+unsigned long lastBuzzerMillis = 0;
 
 int ledState = LOW;
 int buzzerState = LOW;
 
 /* ===== 定数 ===== */
-#define RECEIVE_INTERVAL 200
-#define BLINK_INTERVAL   500
+#define RECEIVE_INTERVAL         200
+#define BLINK_INTERVAL           500
+#define WARNING_BUZZER_INTERVAL  300
 
 unsigned long blinkInterval = BLINK_INTERVAL;
 
@@ -42,6 +46,7 @@ unsigned long readRemote(void);
 void updateState(unsigned long code);
 void controlLed(int state);
 void controlBuzzer(int state);
+void warningMode(int state);
 void stopAll(void);
 
 /* ===== 初期化 ===== */
@@ -60,7 +65,7 @@ void setup() {
   irrecv.enableIRIn();
 
   Serial.println("System Start");
-  Serial.println("1: LED / 2: BUZZER / 3: STOP");
+  Serial.println("1: LED / 2: BUZZER / 3: STOP / WARNING");
 }
 
 /* ===== メイン処理 ===== */
@@ -89,6 +94,9 @@ void loop() {
     ledState = LOW;
 
     controlBuzzer(currentState);
+  }
+  else if (currentState == STATE_WARNING) {
+    warningMode(currentState);
   }
   else if (currentState == STATE_STOP) {
     stopAll();
@@ -150,6 +158,10 @@ void updateState(unsigned long code) {
     currentState = STATE_STOP;
     Serial.println("State: STOP");
   }
+  else if (code == CODE_WARNING) {
+    currentState = STATE_WARNING;
+    Serial.println("State: WARNING");
+  }
   else {
     Serial.println("Unknown Code");
   }
@@ -197,6 +209,49 @@ void controlBuzzer(int state) {
   }
 }
 
+/* ===== 警告モード ===== */
+/* LED点滅とアクティブブザーを同時に動作させる */
+void warningMode(int state) {
+  unsigned long now;
+
+  if (state != STATE_WARNING) {
+    Serial.println("Warning Skip");
+    return;
+  }
+
+  now = millis();
+
+  /* LED点滅制御 */
+  if (now - lastBlinkMillis >= blinkInterval) {
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    digitalWrite(LED_PIN, ledState);
+    lastBlinkMillis = now;
+
+    Serial.print("Warning Mode LED, ledState = ");
+    Serial.println(ledState);
+  }
+
+  /* ブザーON/OFF制御 */
+  if (now - lastBuzzerMillis >= WARNING_BUZZER_INTERVAL) {
+    if (buzzerState == LOW) {
+      buzzerState = HIGH;
+    } else {
+      buzzerState = LOW;
+    }
+
+    digitalWrite(BUZZER_PIN, buzzerState);
+    lastBuzzerMillis = now;
+
+    Serial.print("Warning Mode Buzzer, buzzerState = ");
+    Serial.println(buzzerState);
+  }
+}
+
 /* ===== 停止処理 ===== */
 void stopAll(void) {
   digitalWrite(LED_PIN, LOW);
@@ -209,4 +264,4 @@ void stopAll(void) {
   currentState = STATE_WAIT;
 
   Serial.println("Stop All");
-} 
+}  
